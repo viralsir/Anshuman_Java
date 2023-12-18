d805bb4ad23419bb8c0679237cd9f76571461932
ViralSir <viralsir2018@gmail.com>
2023-12-18 13:56:34 +0530

    Lecture-1

---
+++ src/Firstdemo.java
@@ -0,0 +1,61 @@
+/*
+Sun Microsystem Ltd
+
+       Jdk (Java Developement Kit)  =  Java Compiler +  JVM (Java Virutal Machine)  +  Lib
+
+      Java SE  : Java Standard Edition
+
+      Java EE  : Java Enterprise Edition  (website , Web application,---)  Spring boot
+
+
+
+       Java(2)  Springboot(1)   microservice(1) , spring security(15)
+
+
+
+
+
+      Java ME  : Java Micro Edition (
+
+                           Java SE   (Java)
+
+                              |
+           Java EE                                  Java ME
+
+
+
+        String is a special data type
+
+    to compile
+        javac FirstDemo.java -->    Firstdemo.class
+
+    to run
+         java  Firstdemo
+
+    Firstdemo.main()
+
+   public : it can access from any where
+   static : it can access by object as well as ClassName
+   void : it does not return value
+
+
+    int no[];
+    int[] no;
+
+
+
+ */
+
+public class Firstdemo
+{
+
+
+//    static public  void main(String[] arg)
+//    {
+//         System.out.print("hello \n \t\t ");
+//         System.out.print("Wel come to the world of Java");
+//    }
+
+}
+
+

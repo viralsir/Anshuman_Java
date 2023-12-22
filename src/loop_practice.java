506e9db72bfd467e8f583ac5bddc858182c49838
ViralSir <viralsir2018@gmail.com>
2023-12-22 14:06:27 +0530

    homework of loop started

---
+++ src/loop_practice.java
@@ -0,0 +1,98 @@
+import java.util.Scanner;
+
+public class loop_practice {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+       //Question 1 : -
+
+//        for (int i = 1; i < 11; i++) {
+//            System.out.println(i);
+//        }
+
+
+        //Question 2 : - -------
+//        int anshu = 10;
+//        System.out.print("The first " + anshu + " natural numbers are:\n");
+//        for (int i = 1; i <= anshu; i++) {
+//            System.out.print(i + " ");
+//        }
+//
+//
+//        int sum = Integer.sum(anshu);
+//        System.out.println("\nThe Sum is: " + sum);
+//    }
+
+        //Question 3 : -
+
+//        System.out.println("Enter a number : - ");
+//        int input1 = s.nextInt();
+//
+//        for (int i = 0; i < input1; i++) {
+//            System.out.println(i + 1);
+//        }
+
+        //Question 4 : -
+
+//
+//        int n=10;
+//        int sum=0;
+//        for(int i=1;i<=n;i++)
+//        {
+//            sum=sum+i;
+//        }
+//        System.out.println("The sum of given numbers is : "+sum);
+//        System.out.println("The Average of given numbers is : "+sum/n);
+
+        //Question 5 : -
+
+//        System.out.println("Enter a number : -");
+//        int cube = s.nextInt();
+//
+//        int sum = 0;
+//        for (int i = 0; i < cube; i++) {
+//            sum =  cube * cube * cube;
+//            System.out.println(sum);
+//
+//        }
+
+        //Question 6 : -
+
+//        System.out.println("Enter a number : -");
+//        int table = s.nextInt();
+//
+//        for (int i = 0; i<11 ; i++) {
+//
+//            System.out.println(table + "*" + i + "=" + table * i + "  ");
+//        }
+
+        //Question 7 : -
+
+//        System.out.println("Enter a number : -");
+//        int table = s.nextInt();
+//
+//        for (int i = 0; i<11 ; i++) {
+//
+//            System.out.print(table + "*" + i + "=" + table * i + "  ");
+//        }
+
+        //Question 8 : -
+
+//        System.out.println("Enter a number : -");
+//        int input2 = s.nextInt();
+//        int sample = 0;
+//        int sum = 0;
+//
+//        for (int i = 0; i < input2;i++) {
+//            if (i % 2 != 0){
+//                System.out.println(i);
+//                System.out.println("SUM :" + (sum = sum + i));
+//            }
+//            else {
+//                System.out.println("  ");
+//            }
+//         sample = i;
+//        }
+    }
+
+    }
+
